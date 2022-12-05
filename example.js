e9b3899e2931ed7d69bece3e70faa25f6a323aa5
bang_iyannn <100482628+justnotiyann@users.noreply.github.com>
2022-12-05 23:44:42 +0700

    Create example.js
    omay

---
+++ example.js
@@ -0,0 +1,14 @@
+    const posts = await Posts.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+
+    // get total documents in the Posts collection 
+    const count = await Posts.count();
+
+    // return response with posts, total pages, and current page
+    res.json({
+      posts,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page
+    });
