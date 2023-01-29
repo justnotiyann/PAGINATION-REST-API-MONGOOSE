dbfa01dfa037940ef2e6948946bf92859a7e23cb
bang_iyannn <100482628+justnotiyann@users.noreply.github.com>
2023-01-30 00:21:03 +0700

    Update example.js

--- example.js
+++ example.js
@@ -1,14 +1,16 @@
-    const posts = await Posts.find()
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .exec();
+const { page = 1, limit = 10 } = req.query;
 
-    // get total documents in the Posts collection 
-    const count = await Posts.count();
+const posts = await Posts.find()
+  .limit(limit * 1)
+  .skip((page - 1) * limit)
+  .exec();
 
-    // return response with posts, total pages, and current page
-    res.json({
-      posts,
-      totalPages: Math.ceil(count / limit),
-      currentPage: page
-    });
+// get total documents in the Posts collection
+const count = await Posts.count();
+
+// return response with posts, total pages, and current page
+res.json({
+  posts,
+  totalPages: Math.ceil(count / limit),
+  currentPage: page,
+});
